6482bac35a3e5cf7fb97991c65098b17f1ece64e
Daniel Zhu <daniel.zhu@imt-atlantique.fr>
2023-12-13 18:01:44 +0100

    Add AdamW optimiser

--- src/utils.py
+++ src/utils.py
@@ -50,6 +50,19 @@ def cosanneal_lr_adam(lit_mod, lr, T_max=100, weight_decay=0.):
         "lr_scheduler": torch.optim.lr_scheduler.CosineAnnealingLR(opt, T_max=T_max),
     }
 
+def cosanneal_lr_adamw(lit_mod, lr, T_max=100, weight_decay=0.):
+    opt = torch.optim.AdamW(
+        [
+            {"params": lit_mod.solver.grad_mod.parameters(), "lr": lr},
+            {"params": lit_mod.solver.obs_cost.parameters(), "lr": lr},
+            {"params": lit_mod.solver.prior_cost.parameters(), "lr": lr / 2},
+        ], weight_decay=weight_decay
+    )
+    return {
+        "optimizer": opt,
+        "lr_scheduler": torch.optim.lr_scheduler.CosineAnnealingLR(opt, T_max=T_max),
+    }
+
 def cosanneal_lr_lion(lit_mod, lr, T_max=100):
     import lion_pytorch
     opt = lion_pytorch.Lion(
