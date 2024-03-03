ec6a6ed594a7730c6baea957144e884fa8cf5954
Daniel Zhu <daniel.zhu@imt-atlantique.fr>
2024-03-03 13:38:54 +0100

    Add transfert contrib
    Formerly "comfra" (for "common framework"). This reorganised contribution includes:
    
    -  TransfertDataModule;
    - AdamW optimiser;
    - A load and interpolate function;
    - A run procedure (train+test).

---
+++ contrib/transfert/__init__.py
@@ -0,0 +1,89 @@
+"""
+"""
+import torch
+import xarray as xr
+
+from src.data import AugmentedDataset, BaseDataModule, XrDataset
+
+class TransfertDataModule(BaseDataModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.mean_std_domain = kwargs.get('mean_std_domain', 'train')
+
+    def train_mean_std(self, variable='tgt'):
+        train_data = (
+            self.input_da.sel(self.xrds_kw.get('domain_limits', {}))
+            .sel(self.domains[self.mean_std_domain])
+        )
+        return (
+            train_data
+            .sel(variable=variable)
+            .pipe(lambda da: (da.mean().item(), da.std().item()))
+        )
+
+    def setup(self, stage='test'):
+        post_fn = self.post_fn()
+
+        if stage == 'fit':
+            train_data = self.input_da.sel(self.domains['train'])
+            self.train_ds = XrDataset(
+                train_data, **self.xrds_kw, postpro_fn=post_fn,
+            )
+            if self.aug_kw:
+                self.train_ds = AugmentedDataset(self.train_ds, **self.aug_kw)
+
+            self.val_ds = XrDataset(
+                self.input_da.sel(self.domains['val']),
+                **self.xrds_kw,
+                postpro_fn=post_fn,
+            )
+        else:
+            self.test_ds = XrDataset(
+                self.input_da.sel(self.domains['test']),
+                **self.xrds_kw,
+                postpro_fn=post_fn,
+            )
+
+
+def cosanneal_lr_adamw(lit_mod, lr, T_max, weight_decay=0.):
+    opt = torch.optim.AdamW(
+        [
+            {'params': lit_mod.solver.grad_mod.parameters(), 'lr': lr},
+            {'params': lit_mod.solver.obs_cost.parameters(), 'lr': lr},
+            {'params': lit_mod.solver.prior_cost.parameters(), 'lr': lr / 2},
+        ], weight_decay=weight_decay
+    )
+    return {
+        'optimizer': opt,
+        'lr_scheduler': torch.optim.lr_scheduler.CosineAnnealingLR(
+            opt, T_max=T_max,
+        ),
+    }
+
+def load_and_interpolate(tgt_path, inp_path, tgt_var, inp_var, domain):
+    """
+    Load ground truth `tgt` and apply the satellites observations `inp`.
+    """
+    tgt = xr.open_dataset(tgt_path)[tgt_var].sel(domain)
+    inp = xr.open_dataset(inp_path)[inp_var].sel(domain)
+
+    return (
+        xr.Dataset(
+            dict(input=inp*tgt, tgt=(tgt.dims, tgt.values)),
+            inp.coords,
+        )
+        .transpose('time', 'lat', 'lon')
+        .to_array()
+    )
+
+def run(trainer, train_dm, test_dm, lit_mod, ckpt=None):
+    """
+    Fit and test on two distinct domains.
+    """
+    if trainer.logger is not None:
+        print()
+        print('Logdir:', trainer.logger.log_dir)
+        print()
+
+    trainer.fit(lit_mod, datamodule=train_dm, ckpt_path=ckpt)
+    trainer.test(lit_mod, datamodule=test_dm, ckpt_path='best')
