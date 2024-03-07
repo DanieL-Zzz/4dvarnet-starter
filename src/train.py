9482c7d1b00d2b12c8f39b69655a8d3ba1414937
Daniel Zhu <daniel.zhu@imt-atlantique.fr>
2024-03-07 16:12:04 +0100

    ( 2e relance de base_15x15 )

--- src/train.py
+++ src/train.py
@@ -1,7 +1,7 @@
 import torch
 torch.set_float32_matmul_precision('high')
 
-def base_training(trainer, dm, lit_mod, ckpt='outputs/2024-03-03/18-32-58/base_15x15/checkpoints/val_mse=1.97363-epoch=399.ckpt'):
+def base_training(trainer, dm, lit_mod, ckpt='outputs/2024-03-05/16-21-56/base_15x15/checkpoints/val_mse=1.76913-epoch=736.ckpt'):
     if trainer.logger is not None:
         print()
         print("Logdir:", trainer.logger.log_dir)
