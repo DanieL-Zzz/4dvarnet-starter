38400b764fa659fc47f8a51bd779420775e06f45
Daniel Zhu <daniel.zhu@imt-atlantique.fr>
2025-05-21 11:14:48 +0200

    Remove MSE computed in GF

--- contrib/glorys12/__init__.py
+++ contrib/glorys12/__init__.py
@@ -216,18 +216,6 @@ class Lit4dVarNetIgnoreNaN(Lit4dVarNet):
                 on_epoch=True,  # sync_dist=True,
             )
 
-            if phase == 'val':
-                # Log the loss in Gulfstream
-                loss_gf = self.weighted_mse(
-                    out[:, :, 445:485, 420:460].detach().cpu().data
-                    - batch.tgt[:, :, 445:485, 420:460].detach().cpu().data,
-                    np.ones_like(out[:, :, 445:485, 420:460].detach().cpu().data)
-                )
-                self.log(
-                    f'{phase}_loss_gulfstream', loss_gf, on_step=False,
-                    on_epoch=True,
-                )
-
         return loss, out
 
 
